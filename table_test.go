07dd1a93389a587931b537c76a29a6fb14119926
Rangel Reale <rangelspam@gmail.com>
2023-10-22 09:16:08 -0300

    load initial data

--- table_test.go
+++ table_test.go
@@ -132,3 +132,33 @@ func TestDataMerge(t *testing.T) {
 	assert.Assert(t, newData.Tables["tags"] != data.Tables["tags"], "tables should have been cloned")
 	assert.Assert(t, newData.Tables["tags"] != data2.Tables["tags"], "tables should have been cloned")
 }
+
+func TestDataClone(t *testing.T) {
+	data := &Data{
+		Tables: map[string]*Table{
+			"tags": {
+				ID: "tags",
+				Rows: Rows{
+					Row{Fields: map[string]any{"x": 1}},
+					Row{Fields: map[string]any{"x": 2}},
+				},
+			},
+			"posts": {
+				ID: "posts",
+				Rows: Rows{
+					Row{Fields: map[string]any{"a": 5}},
+					Row{Fields: map[string]any{"a": 3}},
+					Row{Fields: map[string]any{"a": 2}},
+				},
+			},
+		},
+	}
+
+	newData, err := data.Clone()
+	assert.NilError(t, err)
+
+	assert.Assert(t, is.Len(newData.Tables["tags"].Rows, 2))
+	assert.Assert(t, is.Len(newData.Tables["posts"].Rows, 3))
+	assert.Assert(t, newData.Tables["tags"] != data.Tables["tags"], "tables should have been cloned")
+	assert.Assert(t, newData.Tables["posts"] != data.Tables["posts"], "tables should have been cloned")
+}
