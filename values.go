f877fcd29d113dcaf4397789b4e4b8501dc83eb3
Rangel Reale <rangelspam@gmail.com>
2024-11-29 21:09:03 -0300

    valuesget

--- values.go
+++ values.go
@@ -7,11 +7,11 @@ import (
 
 // Values represents a list of the row fields and values, similar to a map[string]any.
 type Values interface {
-	Get(fieldName string) (val any, ok bool)  // gets the value of a field, returning whether the field exists.
-	GetOrNil(fieldName string) any            // gets the value of a field, or nil if the field don't exist.
-	GetDefault(fieldName string, def any) any // gets the value of a field, or a default value if the field don't exist.
-	All(yield func(string, any) bool)         // iterator of all the field values.
-	Len() int                                 // returns the amount of field values.
+	Get(fieldName string) (val any, exists bool) // gets the value of a field, returning whether the field exists.
+	GetOrNil(fieldName string) any               // gets the value of a field, or nil if the field don't exist.
+	GetDefault(fieldName string, def any) any    // gets the value of a field, or a default value if the field don't exist.
+	All(yield func(string, any) bool)            // iterator of all the field values.
+	Len() int                                    // returns the amount of field values.
 }
 
 // ValuesMutable is a Values that can be mutated.
@@ -20,6 +20,17 @@ type ValuesMutable interface {
 	Set(fieldName string, val any)     // sets a field value.
 	Insert(seq iter.Seq2[string, any]) // insert a list of field values.
 	Delete(fieldName ...string)        // delete field values.
+}
+
+// ValuesGet gets a value from values casting to the T type.
+func ValuesGet[T any](values Values, fieldName string) (val T, exists bool, isType bool) {
+	v, ok := values.Get(fieldName)
+	if !ok {
+		var ret T
+		return ret, ok, false
+	}
+	vt, ok := v.(T)
+	return vt, true, ok
 }
 
 // NewValues creates a new ValuesMutable.
@@ -33,8 +44,8 @@ func NewValues(val map[string]any) ValuesMutable {
 // MapValues is a ValuesMutable implementation using a map[string]any
 type MapValues map[string]any
 
-func (v MapValues) Get(fieldName string) (val any, ok bool) {
-	val, ok = v[fieldName]
+func (v MapValues) Get(fieldName string) (val any, exists bool) {
+	val, exists = v[fieldName]
 	return
 }
 
